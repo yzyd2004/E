0bed25249010a9572a89b9dd20b8b0961d9663cc
br5kly <108040523+br5kly@users.noreply.github.com>
2022-10-05 15:24:11 +0200

    Create ALL.py

---
+++ ALL.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import time
+sex = "ﻪﻄﺳﺍﻮﺑ ﻩﺍﺩﻻﺍ ﺍﺬﻫ ﻞﻳﺪﻌﺗ ﻢﺗ \n ﻲﻧﺎﺒﻌﻟﺍ ﺩﺎﻳﺯ\n ﻲﺒﻴﻠﻟﺍ ﻝﺎﺨﻟﺍ \n GRETZ TO OMAR & \n AHMED XR \n WANIS "
+for letter in sex:
+	time.sleep(0.04)
+	sys.stdout.write(letter)
+	sys.stdout.flush()
+os.system('clear')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100000593464379')
+try:
+	import rich
+except ImportError:
+	os.system('pip install rich')
+try:
+	import requests
+except ImportError:
+	os.system('pip install requests')
+
+from rich import print
+from rich.panel import Panel
+print(Panel("Hello, [red]World! \n ALL TOOLS IN ONE \n BY Zeyad Alabany \n TELE : sxtz0 \n facebook : https://www.facebook.com/profile.php?id=100000593464379"))
+from rich.console import Console
+console = Console()
+console.print("[01] JUTTBRAND FILE CLONE",style=" bold white")
+console.print("[02] Darkwolfrabin/DW-07",style=" bold white")
+console.print("[03] Hamii-king-06/Random",style=" bold white")
+console.print("[04] RED-DEMON-ANNOS/7SINS",style=" bold white")
+console.print("[05] TEAM-KRS/BYPASS-JUTT",style=" bold white")
+zeyad = input("CHOOSE")
+if zeyad in ["1","01"]:
+	os.system('rm -rf DUMP-FILE && git clone https://github.com/JUTTBRAND/DUMP-FILE.git && cd DUMP-FILE && python JXB.py')
+if zeyad in ["2","02"]:
+	os.system('git clone https://github.com/Darkwolfrabin/DW-07.git && cd DW-07 && python nep-enc.py')
+if zeyad in ["3", "03"]:
+	os.system('rm -rf Random && git clone https://github.com/Hamii-king-06/Random && cd Random && python3 Hamii.py')
+if zeyad in ["4", "04"]:
+	os.system('git clone https://github.com/RED-DEMON-ANNOS/7SINS  && cd 7SINS && python 7SINS.py')
+if zeyad in ["5", "05"]:
+	os.system('rm -rf BYPASS-JUTT && git clone https://github.com/TEAM-KRS/BYPASS-JUTT.git && cd BYPASS-JUTT && ls && python jutt-brand.py')
